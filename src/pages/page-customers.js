e5dd1c9f4a0716c1f4c918ebe149dcccdb15a3a3
Sean Jacobson <sean.jacobson86@gmail.com>
2023-08-04 14:25:38 -0600

    Made a customer card layout

--- src/pages/page-customers.js
+++ src/pages/page-customers.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+// Components
+import CustomerList from "../components/customerPage/customerList/customerList";
+
 export default function Customers() {
     return (
         <div className="content">
-            This is the customers page!
+            <h1>Customers</h1>
+            <CustomerList />
         </div>
     );
 };
