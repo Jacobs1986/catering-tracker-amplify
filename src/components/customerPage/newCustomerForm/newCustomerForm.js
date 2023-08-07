5cac2036c9d71a5828fe2f9434da53e23160cc55
Sean Jacobson <sean.jacobson86@gmail.com>
2023-08-07 16:52:18 -0600

    Added an animation for showing the customer form

--- src/components/customerPage/newCustomerForm/newCustomerForm.js
+++ src/components/customerPage/newCustomerForm/newCustomerForm.js
@@ -1,4 +1,6 @@
 import React, { 
+    createRef,
+    useState,
     useReducer
 } from "react";
 
@@ -10,6 +12,8 @@ import { reducer as addContactReducer } from "../../../functions/reducer"
 
 export default function NewCustomer() {
     const [customerInfo, setNewCustomerInfo] = useReducer(addContactReducer, {});
+    const [showForm, setShowForm] = useState(false)
+    const testRef = createRef();
 
     // Function to input information into customerInfo
     const handleInputCustomerInfo = event => {
@@ -20,78 +24,87 @@ export default function NewCustomer() {
         })
     }
 
+    // Handle showing the add form
+    const handleShowAddForm = event => {
+        event.preventDefault();
+        setShowForm(!showForm);
+    }
+
     // Function to save the customer information
     const handleSaveCustomer = (event) => {
         event.preventDefault();
-        console.log(customerInfo);
+        testRef.classList.toggle('showCustomerForm');
     }
 
     // Function to cancel saving the information
     const handleCancelSave = event => {
         event.preventDefault();
         setNewCustomerInfo({ reset: true });
+        setShowForm(!showForm);
     }
 
     return (
         <div>
-            <h3>Add New Customer</h3>
+            <button id="addContactButton" onClick={handleShowAddForm}>Add New Customer</button>
             {/* New customer form */}
-            <form className="newCustomerForm">
-                {/* Customer name */}
-                <div className="row">
-                    <div className="col-xs-12 col-s-1 col-lg-1">
-                        <label htmlFor="name">Name:</label>
+            <div>
+                <form className="newCustomerForm" style={!showForm ? { display: "none"} : { display: "block" }}>
+                    {/* Customer name */}
+                    <div className="row">
+                        <div className="col-xs-12 col-s-1 col-lg-1">
+                            <label htmlFor="name">Name:</label>
+                        </div>
+                        <div className="col-xs-12 col-s-10 col-lg-10">
+                            <input type="text" name="name" value={customerInfo.name || ""} onChange={handleInputCustomerInfo} />
+                        </div>
                     </div>
-                    <div className="col-xs-12 col-s-10 col-lg-10">
-                        <input type="text" name="name" value={customerInfo.name || ""} onChange={handleInputCustomerInfo} />
+                    {/* Address */}
+                    <div className="row">
+                        {/* Street */}
+                        <div className="col-xs-12 col-m-1">
+                            <label htmlFor="street">Street:</label>
+                        </div>
+                        <div className="col-xs-12 col-m-5">
+                            <input type="text" name="street" value={customerInfo.street || ""} onChange={handleInputCustomerInfo} />
+                        </div>
+                        <div className="col-m-1"></div>
+                        {/* Apt/Unit */}
+                        <div className="col-xs-12 col-m-1" id="newCustomerAptUnit">
+                            <label htmlFor="unit">Apt/Unit:</label>
+                        </div>
+                        <div className="col-xs-4 col-m-2">
+                            <input type="text" name="unit" value={customerInfo.unit || ""} onChange={handleInputCustomerInfo} />
+                        </div>
                     </div>
-                </div>
-                {/* Address */}
-                <div className="row">
-                    {/* Street */}
-                    <div className="col-xs-12 col-m-1">
-                        <label htmlFor="street">Street:</label>
+                    <div className="row">
+                        {/* City */}
+                        <div className="col-xs-12 col-m-1">
+                            <label htmlFor="city">City:</label>
+                        </div>
+                        <div className="col-xs-12 col-m-3">
+                            <input type="text" name="city" value={customerInfo.city || ""} onChange={handleInputCustomerInfo} />
+                        </div>
+                        {/* State */}
+                        <div className="col-xs-12 col-m-1">
+                            <label htmlFor="state">State:</label>
+                        </div>
+                        <div className="col-xs-3 col-m-1">
+                            <input type="text" name="state" value={customerInfo.state || ""} onChange={handleInputCustomerInfo} />
+                        </div>
+                        {/* Zip */}
+                        <div className="col-xs-12 col-m-1">
+                            <label htmlFor="zip">Zip:</label>
+                        </div>
+                        <div className="col-xs-5 col-m-2">
+                            <input type="text" name="zip" value={customerInfo.zip || ""} onChange={handleInputCustomerInfo} />
+                        </div>
                     </div>
-                    <div className="col-xs-12 col-m-5">
-                        <input type="text" name="street" value={customerInfo.street || ""} onChange={handleInputCustomerInfo} />
+                    <div className="row">
+                        <button className="saveButton" onClick={handleSaveCustomer}>Save</button>
+                        <button className="cancelButton" onClick={handleCancelSave}>Cancel</button>
                     </div>
-                    <div className="col-m-1"></div>
-                    {/* Apt/Unit */}
-                    <div className="col-xs-12 col-m-1" id="newCustomerAptUnit">
-                        <label htmlFor="unit">Apt/Unit:</label>
-                    </div>
-                    <div className="col-xs-4 col-m-2">
-                        <input type="text" name="unit" value={customerInfo.unit || ""} onChange={handleInputCustomerInfo} />
-                    </div>
-                </div>
-                <div className="row">
-                    {/* City */}
-                    <div className="col-xs-12 col-m-1">
-                        <label htmlFor="city">City:</label>
-                    </div>
-                    <div className="col-xs-12 col-m-3">
-                        <input type="text" name="city" value={customerInfo.city || ""} onChange={handleInputCustomerInfo} />
-                    </div>
-                    {/* State */}
-                    <div className="col-xs-12 col-m-1">
-                        <label htmlFor="state">State:</label>
-                    </div>
-                    <div className="col-xs-3 col-m-1">
-                        <input type="text" name="state" value={customerInfo.state || ""} onChange={handleInputCustomerInfo} />
-                    </div>
-                    {/* Zip */}
-                    <div className="col-xs-12 col-m-1">
-                        <label htmlFor="zip">Zip:</label>
-                    </div>
-                    <div className="col-xs-5 col-m-2">
-                        <input type="text" name="zip" value={customerInfo.zip || ""} onChange={handleInputCustomerInfo} />
-                    </div>
-                </div>
-                <div className="row">
-                    <button className="saveButton" onClick={handleSaveCustomer}>Save</button>
-                    <button className="cancelButton" onClick={handleCancelSave}>Cancel</button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     );
 };
