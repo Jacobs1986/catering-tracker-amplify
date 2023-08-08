de3f5f746820757df677ac6703781c4e4fa2d612
Sean Jacobson <sean.jacobson86@gmail.com>
2023-08-08 11:11:07 -0600

    Added tbody and thead to the code to remove error

--- src/components/customerPage/customerTable/customerTable.js
+++ src/components/customerPage/customerTable/customerTable.js
@@ -14,20 +14,24 @@ export default function CustomerTable() {
         <div>
             {customer.length === 0 ? <div><h1>No Customers</h1></div> : 
                 <table className="customerTable">
-                    <tr>
-                        <th>Customer Name</th>
-                        <th>Total Orders</th>
-                        <th>Last Order</th>
-                        <th>Order Total</th>
-                    </tr>
-                    {customer.map((customer, i) => (
-                        <tr key={i}>
-                            <td>{customer.name}</td>
-                            <td>{customer.totalOrders}</td>
-                            <td>{customer.lastOrder}</td>
-                            <td>{customer.orderTotal}</td>
+                    <thead>
+                        <tr>
+                            <th>Customer Name</th>
+                            <th>Total Orders</th>
+                            <th>Last Order</th>
+                            <th>Order Total</th>
                         </tr>
+                    </thead>
+                    <tbody>
+                        {customer.map((customer, i) => (
+                            <tr key={i}>
+                                <td>{customer.name}</td>
+                                <td>{customer.totalOrders}</td>
+                                <td>{customer.lastOrder}</td>
+                                <td>{customer.orderTotal}</td>
+                            </tr>
                     ))}
+                    </tbody>
                 </table>
             }
         </div>
