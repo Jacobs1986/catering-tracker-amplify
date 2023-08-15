29986ea866d8b39fc9de05525573b2fbb2188faf
Sean Jacobson <sean.jacobson86@gmail.com>
2023-08-14 18:54:49 -0600

    Started to create the new items form; breaking it up into components to make it easier to work with

---
+++ src/components/menuPage/newItemForm/mainItemForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./newItemForm.css";
+
+export default function MainItemForm() {
+    return (
+        <div>This is the form that will appear if the main item raido is checked.</div>
+    );
+};
--- src/components/menuPage/newItemForm/newItemForm.js
+++ src/components/menuPage/newItemForm/newItemForm.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, {
+    useState
+} from "react";
 
 // CSS File
 import "./newItemForm.css";
 
+// Import components
+import MainItemForm from "./mainItemForm";
+import AddonItemForm from "./addonItemForm";
+
 export default function NewItemForm() {
+    const [radioCheck, setRadioCheck] = useState("");
+
+    // Function that will show the different forms
+    const handleShowForm = (event) => {
+        setRadioCheck(event.target.value)
+    }
+
     return (
         <div>
+            {/* Button that will show the form */}
             <button>Add New Item</button>
+            {/* New Item form */}
             <form className="formNewItem">
                 <div className="row">
-                    {/* Item name */}
-                    <div className="col-xs-12 col-s-4">
-                        <label htmlFor="itemName">Name:</label>
-                        <input type="text" name="itemName" />
-                    </div>
-                    {/* Item type */}
-                    <div className="col-xs-12 col-s-4">
-                        <label htmlFor="itemType">Type:</label>
-                        <select name="itemType">
-                            <option value="main">Main</option>
-                            <option value="side">Side</option>
-                            <option value="drink">Drink</option>
-                            <option value="dessert">Dessert</option>
-                        </select>
-                    </div>
-                    {/* Attributes */}
+                    {/* Radio buttons that will show either a main item form or add on form */}
                     <div className="col-xs-12">
-
+                        Is this a main item or an add on?
                     </div>
+                    <div className="col-xs-12">
+                        <input type="radio" id="mainItem" name="item_type" value="mainItem" checked={radioCheck === "mainItem"} onChange={handleShowForm} />
+                        <label htmlFor="mainItem">Main Item</label>
+                        <input type="radio" id="addOn" name="item_type" value="addOn" checked={radioCheck === "addOn"} onChange={handleShowForm} />
+                        <label htmlFor="addOn">Add-on</label>
+                    </div>
+                </div>
+                {/* The forms */}
+                <div>
+                    {radioCheck === "mainItem" ? 
+                        <MainItemForm /> :
+                    radioCheck === "addOn" ? 
+                        <AddonItemForm /> :
+                        <div></div>
+                    }
                 </div>
             </form>
         </div>
