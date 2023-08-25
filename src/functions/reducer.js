166e1b10d3158e6bc71ebe4ee6810cbe7a5e4a36
Sean Jacobson <sean.jacobson86@gmail.com>
2023-08-25 13:15:22 -0600

    Created a new reducer that will handle the sizes

--- src/functions/reducer.js
+++ src/functions/reducer.js
@@ -1,4 +1,4 @@
-// Input reducer
+// Generic Input reducer
 export const reducer =  (state, action) => {
     // Begin switch statement
     switch (action.type) {
@@ -28,5 +28,36 @@ export const reducer =  (state, action) => {
         }
         default:
             throw Error('Unknown action: ' + action.type);
+    }
+}
+
+// Reducer for size Info
+export const sizeReducer = (state, action) => {
+    // Begin the switch statement
+    switch (action.type) {
+        // If the type is add
+        case 'add': {
+            // add to the state
+            return {
+                ...state,
+                [action.name]: action.value
+            };
+        }
+         // If the type is delete
+         case 'delete': {
+            // Set the removeName variable
+            let removeName = "sizeType"+action.num;
+            // Set the removeCost variable
+            let removeCost = "sizeCost"+action.num;
+            // delete removeName from state
+            delete state[removeName];
+            // delete removeCost from state
+            delete state[removeCost];
+            // return the state
+            return state;
+         }
+         // If the type is unknown
+         default:
+            throw Error('Unkown action: ' + action.type);
     }
 }
